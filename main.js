4163bf4fd5f1368e8af1279fec8e0cd29bba0712
tommaso <scocciolinitommaso@gmail.com>
2021-03-23 15:33:44 +0100

    esercizio completato +bonus

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("ok");
+
+var app = new Vue(
+  {
+    el: "#root",
+    data: {
+      message: "Sono un messaggio tramite data",
+      image: "https://picsum.photos/200/300",
+    }
+  }
+);
